d33280ad33a2f7362e02fe8681e41e7d8662ccac
Chantal Neo <Chantal@hotmail.sg>
2020-03-26 17:32:05 +0800

    Quiz 2: Test Your Knowledge: Variable Assignment

--- main.go
+++ main.go
@@ -25,3 +25,60 @@ func main() {
 //
 // 3. Only use := syntax when you're defining a new variable. To resign a new value to in this case, our variable "card", simply use the following:
 //    card = "Five of Diamonds"
+
+// Quiz 2: Test Your Knowledge: Variable Assignment
+// 1. Is the following a valid way of initializing and assigning a value to a variable? Yes
+// var bookTitle string = "Harry Potter"
+// 2. Is the following a valid way of initializing an assigning a value to a variable? Yes
+// fruitCount := 5
+// 3. After running the following code, Go will assume that the variable quizQuestionCount is of what type? Integer
+// quizQuestionCount := 10
+// 4. Will the following code compile?  Why or why not? No, because a variable can only be initialized one time. In this case, the ':=' operator is being used to intialize 'paperColor' twice
+// paperColor := "Green"
+// paperColor := "Blue"
+// 5. Are the two lines following ways of initializing the variable 'pi' equivalent? Yes
+// pi := 3.14
+// var pi float = 3.14
+// 6. This might require a bit of experimentation on your end :)
+// Remember that you can use the Go Playground at https://play.golang.org/ to quickly run a snippet of code
+// Is the following code valid? Due to a syntax error: non-declaration statement outside function body that'd occur, it's a no
+/* 	package main
+
+	import "fmt"
+	deckSize := 20
+
+	func main() {
+  		fmt.Println(deckSize)
+	} */
+// 7. We might be able to initialize a variable and then later assign a variable to it. Is the following code valid? Yes
+/*	package main
+
+	import "fmt"
+
+	func main() {
+  		var deckSize int
+  		deckSize = 52
+  	fmt.Println(deckSize)
+	} */
+// 8. Here's another one that might need some testing on your end!
+// Remember that you can use the Go Playground at https://play.golang.org/ to quickly run a snippet of code.
+// Is the following code valid? Yes, we can initialize a variable outside of a function, we just can't assign a value to it
+/* 	package main
+
+	import "fmt"
+
+	var deckSize int
+
+	func main() {
+  		deckSize = 50
+  		fmt.Println(deckSize)
+	} */
+// 9. Is the following code valid? Why or why not? No. This is because, variables must first be initialized with the ':=' operator or the 'var variableName type' syntax
+/* 	package main
+
+	import "fmt"
+
+	func main() {
+ 		deckSize = 52
+  		fmt.Println(deckSize)
+	} */
