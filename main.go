3523a9ea6d47e58eae445253ea43f9e5c9ccfc01
Chantal Neo <Chantal@hotmail.sg>
2020-03-26 19:54:55 +0800

    Functions and Return Types

--- main.go
+++ main.go
@@ -4,9 +4,15 @@ import "fmt"
 
 func main() {
 	// var card string = "Ace of Spades" // Breaking down of the syntax, check point 1 below
-	card := "Ace of Spades"   // This is the 100% equivalent of the code right above. Here we rely upon the compiler to infer that card is supposed to contain a string
-	card = "Five of Diamonds" // Important note on point 3 below
+	// card := "Ace of Spades" // This is the 100% equivalent of the code right above. Here we rely upon the compiler to infer that card is supposed to contain a string
+	// card = "Five of Diamonds" // Important note on point 3 below
+	card := newCard()
+
 	fmt.Println(card)
+}
+
+func newCard() string { // Breaking down of the syntax, check point 4 below
+	return "Five of Diamonds"
 }
 
 // Notes:
@@ -25,6 +31,12 @@ func main() {
 //
 // 3. Only use := syntax when you're defining a new variable. To resign a new value to in this case, our variable "card", simply use the following:
 //    card = "Five of Diamonds"
+//
+// 4. func newCard() string {
+//	     return "Five of Diamonds"
+//    }
+//    newCard there helps define a function called 'newCard'
+//    string there tells go that when executed, this function will return a value of type 'string'
 
 // Quiz 2: Test Your Knowledge: Variable Assignment
 // 1. Is the following a valid way of initializing and assigning a value to a variable? Yes
